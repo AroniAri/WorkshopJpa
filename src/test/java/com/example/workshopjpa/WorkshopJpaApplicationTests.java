16a8b1756c45d9ee35bf9608d8aeca6d640c8b65
AroniAri <meriton12345@gmail.com>
2024-09-19 09:59:15 +0200

    Initial commit

---
+++ src/test/java/com/example/workshopjpa/WorkshopJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshopjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
