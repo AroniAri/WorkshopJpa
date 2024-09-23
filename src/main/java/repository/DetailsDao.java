3568a30befb3cc20522930bff7c642f9d9fc9309
AroniAri <meriton12345@gmail.com>
2024-09-23 22:53:49 +0200

    Part 2

--- src/main/java/repository/DetailsDao.java
+++ src/main/java/repository/DetailsDao.java
@@ -1,4 +1,14 @@
 package repository;
 
-public interface DetailsDao {
+import entities.Details;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface DetailsDao extends CrudRepository<Details, Integer> {
+
+    Optional<Details> findByEmail(String email);
+    Optional<Details> findByNameContaining(String name);
+    Optional<Details> findByNameIgnoreCase(String name);
+    Optional<Details> findByBirthDate(String birthDate);
 }
